b3ee64e9a72c414fc3bf04637e44b7b2b33a4d44
Nayara Souza <nayara.souza@ccc.ufcg.edu.br>
2022-04-24 23:22:31 -0300

    Criado view para os agendamentos

---
+++ src/pages/Agendamentos/index.js
@@ -0,0 +1,56 @@
+import {useCallback, useEffect, useState} from "react";
+import {Check, X} from "tabler-icons-react";
+import './style.css'
+
+import axios from '../../services/api.js'
+
+
+const Agendamentos = (columns,
+                      endpoint,
+                      title,
+                      refetchTimestamp) => {
+    const [agendamentos, setAgendamentos] = useState([]);
+
+    const fetchData = useCallback(() => {
+        axios.get("/agendamentos").then((response) => setAgendamentos(response.data.agendamentos));
+    }, []);
+
+    useEffect(() => {
+        fetchData();
+    }, [fetchData, endpoint, refetchTimestamp]);
+
+
+
+        return (
+            <>
+                {Object.keys(agendamentos).map((key) =>
+                    <div className="table">
+                        <p>{key}:</p>
+                            <div>
+                                <table>
+                                    <tr>
+                                        <th>Nome</th>
+                                        <th>Data de Nascimento</th>
+                                        <th>Data Agendada</th>
+                                        <th>Hora Agendada</th>
+                                        <th>Status</th>
+                                    </tr>
+                                    {agendamentos[key].map((valor) =>
+                                        <tr>
+                                            <th>{valor.nome}</th>
+                                            <th>{valor.dataNascimento}</th>
+                                            <th>{valor.dataAgendada}</th>
+                                            <th>{valor.horaAgendada}</th>
+                                            <th>{valor.status? <Check/>: <X/>}</th>
+                                        </tr>
+                                    )}
+                                </table>
+                            </div>
+                    </div>
+                )}
+            </>
+        )
+
+}
+
+export default Agendamentos;
